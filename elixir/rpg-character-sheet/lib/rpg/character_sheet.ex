e7b1c455e86c84d2fc4a25953fc57b3bea7ff81f
ellnix <103502144+ellnix@users.noreply.github.com>
2024-03-25 21:43:52 +0100

    Solve elixir/rpg-character-sheet

---
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -0,0 +1,29 @@
+defmodule RPG.CharacterSheet do
+  def welcome() do
+    IO.puts "Welcome! Let's fill out your character sheet together."
+  end
+
+  def ask_name() do
+    String.trim(IO.gets("What is your character's name?\n"))
+  end
+
+  def ask_class() do
+    String.trim(IO.gets("What is your character's class?\n"))
+  end
+
+  def ask_level() do
+    elem(Integer.parse(IO.gets("What is your character's level?\n")), 0)
+  end
+
+  def run() do
+    welcome()
+    character = %{
+      name: ask_name(),
+      class: ask_class(),
+      level: ask_level(),
+    }
+
+    IO.write "Your character: "
+    IO.inspect character
+  end
+end
