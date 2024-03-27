b4ba4b4fe66d9e20a41062beda1bd12762886588
ellnix <103502144+ellnix@users.noreply.github.com>
2024-03-27 14:45:33 +0100

    Solve elixir/newsletter

---
+++ elixir/newsletter/lib/newsletter.ex
@@ -0,0 +1,33 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    with {:ok, content}  <- File.read(path) do
+      content
+        |> String.split("\n")
+        |> Enum.reject(& &1 == "")
+    end
+  end
+
+  def open_log(path) do
+    with {:ok, pid} <- File.open(path, [:write]), do: pid
+  end
+
+  def log_sent_email(pid, email) do
+    IO.write(pid, "#{email}\n")
+  end
+
+  def close_log(pid) do
+    File.close(pid)
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    log = open_log(log_path)
+    Enum.each(read_emails(emails_path), fn email -> send_one(email, log, send_fun) end)
+    close_log(log)
+  end
+
+  defp send_one(email, log, send_fun) do
+    with :ok <- send_fun.(email) do
+      log_sent_email(log, email)
+    end
+  end
+end
