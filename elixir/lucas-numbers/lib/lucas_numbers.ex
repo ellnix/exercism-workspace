07296b563c8b8a68da6f8d04145a82c91f95b20d
ellnix <103502144+ellnix@users.noreply.github.com>
2024-04-04 14:50:53 +0200

    Solve elixir/lucas-numbers iteration 2

--- elixir/lucas-numbers/lib/lucas_numbers.ex
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -5,13 +5,13 @@ defmodule LucasNumbers do
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
-  def generate(1), do: [2]
-  def generate(2), do: [2, 1]
   def generate(count) when is_integer(count) and count > 0 do
-    result  = Stream.unfold({2, 1}, fn {a, b} -> {a + b, {b, a + b}}  end)
-    |> Enum.take(count - 2)
-
-    [2, 1 | result]
+    Stream.unfold({}, fn 
+      {} -> {2, {2}}
+      {2} -> {1, {2, 1}}
+      {a, b} -> {a + b, {b, a + b}}  
+    end)
+    |> Enum.take(count)
   end
 
   def generate(_), do: raise(ArgumentError, "count must be specified as an integer >= 1")
