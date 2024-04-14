38cc43debb9e19e1b9874b19b2d2d2affbff40aa
ellnix <103502144+ellnix@users.noreply.github.com>
2024-04-14 13:42:37 +0200

    Solve rust/allergies

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,47 @@
+pub struct Allergies {
+    allergens: Vec<Allergen>,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Allergen {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+}
+
+const ALLERGEN_COUNT: usize = 8;
+
+const ALL_ALLERGENS: [Allergen; ALLERGEN_COUNT] = [
+    Allergen::Eggs,
+    Allergen::Peanuts,
+    Allergen::Shellfish,
+    Allergen::Strawberries,
+    Allergen::Tomatoes,
+    Allergen::Chocolate,
+    Allergen::Pollen,
+    Allergen::Cats,
+];
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self {
+            allergens: (0..ALLERGEN_COUNT)
+                .filter(|i| 2u32.pow(*i as u32) & score != 0)
+                .map(|i| ALL_ALLERGENS[i as usize])
+                .collect(),
+        }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        self.allergens.contains(allergen)
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        self.allergens.clone()
+    }
+}
