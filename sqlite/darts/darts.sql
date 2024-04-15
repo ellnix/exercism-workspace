d6cbb067e3234c8e435da654903290b7d979bb15
ellnix <103502144+ellnix@users.noreply.github.com>
2024-04-15 17:36:26 +0200

    Solve sqlite/darts with a CASE

--- sqlite/darts/darts.sql
+++ sqlite/darts/darts.sql
@@ -2,7 +2,12 @@
 -- Task: update the darts table and set the score based on the x and y values.
 
 UPDATE darts
-  SET score = 0;
+  SET score = CASE 
+  WHEN x * x + y * y <= 100 THEN 1
+  WHEN x * x + y * y <=  25 THEN 5
+  WHEN x * x + y * y <=   1 THEN 10
+  ELSE 0
+END;
 
 UPDATE darts
   SET score = 1
