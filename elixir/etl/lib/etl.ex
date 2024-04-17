95fa2d8a8f24b5a8d862bd4bf846918c4689441a
ellnix <103502144+ellnix@users.noreply.github.com>
2024-04-17 20:55:42 +0200

    Solve elixir/etl

---
+++ elixir/etl/lib/etl.ex
@@ -0,0 +1,14 @@
+defmodule ETL do
+  @doc """
+  Transforms an old Scrabble score system to a new one.
+
+  ## Examples
+
+    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
+    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    for score <- Map.keys(input), letter <- input[score], into: %{}, do: {String.downcase(letter), score}
+  end
+end
