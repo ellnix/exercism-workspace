d9c9d6632345397c0145afc4bdb431e856404b47
ellnix <103502144+ellnix@users.noreply.github.com>
2024-04-17 16:14:18 +0200

    Solve elixir/collatz-conjecture with tail-call opt

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -6,7 +6,9 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(1), do: 0
-  def calc(n) when is_integer(n) and n > 1 and rem(n, 2) == 1, do: 1 + calc(3 * n + 1)
-  def calc(n) when is_integer(n) and n > 1, do: 1 + calc(div(n, 2))
+  def calc(n) when is_integer(n) and n > 0, do: collatz_steps(n, 0)
+
+  defp collatz_steps(1, acc), do: acc
+  defp collatz_steps(n, acc) when rem(n, 2) == 1, do: collatz_steps(3 * n + 1, acc + 1)
+  defp collatz_steps(n, acc) , do: collatz_steps(div(n, 2), acc + 1)
 end
