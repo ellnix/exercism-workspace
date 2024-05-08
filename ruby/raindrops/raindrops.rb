d9a6480de240dcf3c75dc558db6640c6d4e97ca9
KOTP <keeperotphones@gmail.com>
2024-05-08 19:27:41 -0400

    Reduce interim object

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,19 +1,12 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
-
 class Raindrops
+
   def self.convert(n)
     output = ''
-
-    output += 'Pling' if n % 3 == 0
-    output += 'Plang' if n % 5 == 0
-    output += 'Plong' if n % 7 == 0
+    output << 'Pling' if (n % 3).empty?
+    output << 'Plang' if (n % 5).empty?
+    output << 'Plong' if (n % 7).empty?
 
     output == '' ? n.to_s : output
   end
+
 end
