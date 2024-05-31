777f56c1769ca883a376f85d4dfba8f3627585cb
ellnix <ellnix@disroot.org>
2024-05-31 10:29:19 +0200

    Solve c/collatz-conjecture cleaned

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -4,11 +4,13 @@ int steps(int start) {
   if (start < 1)
     return ERROR_VALUE;
 
-  if (start == 1)
-    return 0;
+  int steps;
 
-  if (start % 2 == 0)
-    return steps(start / 2) + 1;
+  for(steps = 0; start > 1; steps++)
+    if (start & 1)
+      start = start * 3 + 1;
+    else
+      start >>= 1;
 
-  return steps(start * 3 + 1) + 1;
+  return steps;
 }
